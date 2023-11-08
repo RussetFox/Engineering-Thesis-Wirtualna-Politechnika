b3611e0927fc05ac2982360353fd001e0631535a
RussetFox <kapi.piejko@gmail.com>
2023-11-08 01:23:49 +0100

    Alpha 1.3.2: Partially added styling to login page

--- wirtualna-front/src/Components/Login.tsx
+++ wirtualna-front/src/Components/Login.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import '../Styles/Login-Page.css';
 export default function Login() {
-  const [login, setLogin] = useState(() => {return ''});
-  const [password, setPassword] = useState(() => {return ''});
+  const [login, setLogin] = useState(() => { return '' });
+  const [password, setPassword] = useState(() => { return '' });
 
   const handleLogin = (event: any) => {
     event.preventDefault();
@@ -11,33 +11,41 @@ export default function Login() {
   };
 
   return (
-    <div className='login-form-frame'>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Nazwa uzytkownika / E-mail:</label>
-          <input
-            type="login"
-            placeholder="Wprowadź nazwę użytkownika lub e-mail"
-            value={login}
-            onChange={(e) => setLogin(e.target.value)}
-            required
-          />
+    <div className='login-page'>
+      <div className='login-form-frame'>
+        <div className='login-form-data'>
+          <h1>Zaloguj się</h1>
+          <form onSubmit={handleLogin}>
+            <div>
+              <label>Nazwa uzytkownika / E-mail:</label>
+              <input
+                type="login"
+                placeholder="Wprowadź nazwę użytkownika lub e-mail"
+                value={login}
+                max={16}
+                onChange={(e) => setLogin(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label>Hasło:</label>
+              <input
+                type="password"
+                placeholder="Wprowadź hasło"
+                value={password}
+                maxLength={32}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Zaloguj się</button>
+          </form>
         </div>
-        <div>
-          <label>Hasło:</label>
-          <input
-            type="password"
-            placeholder="Wprowadź hasło"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+        <div className='login-form-joinus'>
+          <p>Nie masz jeszcze konta?</p>
+          <Link to='/register'>Dołącz do nas!</Link>
         </div>
-        <button type="submit">Zaloguj się</button>
-      </form>
-      <p>Nie masz jeszcze konta?</p>
-      <Link to = '/register'>Dołącz do nas!</Link>
+      </div>
     </div>
   );
 };
