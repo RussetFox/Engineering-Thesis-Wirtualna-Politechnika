1985cdee4805861463dc79afe0310ed8b18bda91
RussetFox <kapi.piejko@gmail.com>
2023-11-09 23:33:29 +0100

    Alpha 1.3.4: Added styling for register page

--- wirtualna-front/src/Components/Register.tsx
+++ wirtualna-front/src/Components/Register.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import '../Styles/Register-Page.css'
+import Button from './Button'
 import { Link } from 'react-router-dom';
 
-export default function Register () {
+export default function Register() {
   const [login, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -9,63 +11,67 @@ export default function Register () {
 
   const handleRegister = (e) => {
     e.preventDefault();
-    if(password !== passwordRepeat) {
-        alert("Hasła nie są takie same");
-        return;
+    if (password !== passwordRepeat) {
+      alert("Hasła nie są takie same");
+      return;
     }
     console.log('Zarejestrowano:', login, email, password);
   };
 
   return (
     <div className='gradient-background-purple'>
-    <div>
-      <h2>Register</h2>
-      <form onSubmit={handleRegister}>
-        <div>
-          <label>Login:</label>
-          <input
-            type="text"
-            placeholder="Wprowadź nazwę użytkownika"
-            value={login}
-            onChange={(e) => setName(e.target.value)}
-            required = {true}
-          />
+      <div className='register-form-frame'>
+        <div className='register-form-data'>
+          <h1>Zarejestruj się!</h1>
+          <form onSubmit={handleRegister}>
+            <div>
+              <label>Login:</label>
+              <input
+                type="text"
+                placeholder="Wprowadź nazwę użytkownika"
+                value={login}
+                onChange={(e) => setName(e.target.value)}
+                required={true}
+              />
+            </div>
+            <div>
+              <label>E-mail:</label>
+              <input
+                type="email"
+                placeholder="Wprowadź adres e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label>Hasło</label>
+              <input
+                type="password"
+                placeholder="Wprowadź hasło"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label>Powtórz hasło</label>
+              <input
+                type="password"
+                placeholder="Powtórz hasło"
+                value={passwordRepeat}
+                onChange={(e) => setPasswordRepeat(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Zarejestruj się</button>
+          </form>
         </div>
-        <div>
-          <label>E-mail:</label>
-          <input
-            type="email"
-            placeholder="Wprowadź adres e-mail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+        <div className='register-form-login'>
+          <p>Jesteś już częścią naszej społeczności?</p>
+          <Button text='Zaloguj się!' color='login-to-button' pagePath = '/login' />
         </div>
-        <div>
-          <label>Hasło</label>
-          <input
-            type="password"
-            placeholder="Wprowadź hasło"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Powtórz hasło</label>
-          <input
-            type="password"
-            placeholder="Powtórz hasło"
-            value={passwordRepeat}
-            onChange={(e) => setPasswordRepeat(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Zarejestruj się</button>
-      </form>
-      <p>Masz już konto?</p>
-      <Link to = '/login'>Zaloguj się!</Link>
-    </div>
+      </div>
     </div>
   );
 };
